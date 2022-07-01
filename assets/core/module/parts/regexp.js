1be7a5551794898be071bb53d455f52b193e24f3
kimson <xpfjrnseks1@naver.com>
2022-07-01 21:41:34 +0900

    파일트리 폰트 조정 및 디렉토리, 파일 분류에 따른 이모지 표시/제거 기능 추가

--- assets/core/module/parts/regexp.js
+++ assets/core/module/parts/regexp.js
@@ -6,9 +6,9 @@
  * @author    kimson <chaplet01@gmail.com>
  * @github    https://github.com/kkn1125
  * @written   2022-04-19 13:07:01
- * @modified  2022-05-29 20:30:46
+ * @modified  2022-07-01 21:38:45
  * @since     v0.1.0
- * @currently v0.2.3
+ * @currently v0.2.4
  */
 
 "use strict";
--- assets/src/script/treeing.js
+++ assets/src/script/treeing.js
@@ -1,46 +1,45 @@
 /**!
  * Copyright 2022. kkn1125 All rights reserved.
- * 
+ *
  * 파일 트리 파싱 : demo 페이지 구동 자바스크립트
- * 
+ *
  * @author    kimson <chaplet01@gmail.com>
  * @github    https://github.com/kkn1125
  * @written   2022-04-19 13:07:01
- * @modified  2022-05-29 20:16:55
+ * @modified  2022-07-01 21:38:45
  * @since     v0.1.0
- * @currently v0.2.3
+ * @currently v0.2.4
  */
 
 "use strict";
 
-import {
-    OptionalParser
-} from "../../core/parser.js";
+import { OptionalParser } from "../../core/parser.js";
 
 /**
  * @since v0.2.2
  */
 const options = {
-    app: "#app",
-    branches: {
-        first: {
-            only: "└",
-            brother: "├",
-        },
-        second: {
-            only: "─",
-            child: "┬",
-        },
-        third: "─",
-        vertical: "│",
+  app: "#app",
+  branches: {
+    first: {
+      only: "└",
+      brother: "├",
     },
-    style: {
-        directory: ["badge", "bg-info"],
-        offset: 1,
-        fontSize: 16
+    second: {
+      only: "─",
+      child: "┬",
     },
-    indent: 1
+    third: "",
+    vertical: "│",
+  },
+  style: {
+    directory: ["badge", "bg-info"],
+    offset: 1,
+    fontSize: 16,
+  },
+  emoji: null,
+  indent: 1,
 };
 
-const TreeParser    = OptionalParser.init(options);
-const parser        = new TreeParser();
+const TreeParser = OptionalParser.init(options);
+const parser = new TreeParser();
